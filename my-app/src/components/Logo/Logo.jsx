aeae37220b66d703bbc4692f1b64ce0e5aac3a2f
Harsh Vaghela <harshvaghela7117@gmail.com>
2024-01-26 16:17:48 +0530

    Nav and hero added

--- my-app/src/components/Logo/Logo.jsx
+++ my-app/src/components/Logo/Logo.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-// import styles from "./Logo.module.css"
+import styles from "./Logo.module.css"
 
 import logo from "../../assets/logo.png"
 
 export default function Logo (){
     return (
 
-        <>
+        <div className={styles.logo}>
             <img src={logo} alt="logo-img" width={67} />
-        </>
+        </div>
     )
 }
